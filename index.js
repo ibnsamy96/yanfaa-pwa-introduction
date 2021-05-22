106fd3702e8e8d1c15137021b15d9159dd5a2b6b
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2021-05-22 02:19:27 +0200

    added index.js code

---
+++ index.js
@@ -0,0 +1,36 @@
+const installBTN = document.querySelector("#installBTN");
+const installedText = document.querySelector("#installedText");
+
+installBTN.style.display = "none";
+installedText.style.display = "none";
+
+if ("serviceWorker" in navigator) {
+  window.navigator.serviceWorker
+    .register("service-worker.js")
+    .then((result) => {
+      console.log(result);
+      console.log("service-worker installed");
+    });
+}
+
+let eventPrompt;
+
+document.addEventListener("beforeinstallprompt", (installEvent) => {
+  installEvent.preventDefault(); // to prevent default installation prompt in some chrome versions
+  eventPrompt = installEvent; // save it in a variable to use it later
+  console.log(eventPrompt);
+
+  installBTN.style.display = "block";
+
+  installBTN.addEventListener("click", (clickEvent) => {
+    eventPrompt.prompt();
+    eventPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accept") {
+        console.log("userAccepted");
+      } else {
+        console.log("userRefused");
+      }
+      eventPrompt = null;
+    });
+  });
+});
